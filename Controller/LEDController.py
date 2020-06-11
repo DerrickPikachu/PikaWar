f28314a8ffa77f0a3de2d99f5c6aa6796a8ae82a
PikaChin <derrfgh4563@gmail.com>
2020-06-11 15:53:14 +0800

    Led controller

---
+++ Controller/LEDController.py
@@ -0,0 +1,27 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+
+class LEDController:
+    def __init__(self):
+        self.ledMap = [[16, 20, 21], [13, 19, 26]]
+
+        for row in self.ledMap:
+            for col in row:
+                GPIO.setup(col, GPIO.OUT)
+
+    def hintLed(self, row, col):
+        GPIO.output(self.ledMap[row][col], 1)
+        time.sleep(2)
+        GPIO.output(self.ledMap[row][col], 0)
+
+
+if __name__ == "__main__":
+    ledController = LEDController()
+
+    for row in range(2):
+        for col in range(3):
+            ledController.hintLed(row, col)
