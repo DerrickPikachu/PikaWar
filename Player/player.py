6fe4760fa793cbb8364a03d0a69ea7f2ef0494aa
PikaChin <derrfgh4563@gmail.com>
2020-06-15 19:54:44 +0800

    player move function

--- Player/player.py
+++ Player/player.py
@@ -3,8 +3,9 @@ class player:
         self.name = name
         self.blood = 10
         self.power = 10
-        self.defense  = 10
+        self.defense = 10
         self.position = [0, 0]
+
     def getName(self)->str:
         return self.name
     
@@ -17,3 +18,31 @@ class player:
     def getDefense(self)->int:
         return self.defense
 
+    def move(self, action):
+        moveVector = [0, 0]
+        if action == 'up':
+            moveVector = [-1, 0]
+        elif action == 'down':
+            moveVector = [1, 0]
+        elif action == 'right':
+            moveVector = [0, 1]
+        elif action == 'left':
+            moveVector = [0, -1]
+
+        temPos = [0, 0]
+        temPos[0] = moveVector[0] + self.position[0]
+        temPos[1] = moveVector[1] + self.position[1]
+
+        if temPos[0] <= 1 and temPos[0] >= 0 and temPos[1] <= 2 and temPos[1] >= 0:
+            self.position = temPos
+        else:
+            # TODO: Raise an exception
+            print("move error!!")
+
+
+if __name__ == "__main__":
+    playerTest = player("test")
+    while True:
+        string = input("action: ")
+        playerTest.move(action=string)
+        print(playerTest.position)
--- server.py
+++ server.py
@@ -17,31 +17,32 @@ def index():
             session["username"] = "player1"
             playerpool["player1"] = player("player1")
             print("born player1")
-            print("pn:",playerpool["player1"].getName())
-            return render_template("index.html",user=playerpool["player1"])
+            print("pn:", playerpool["player1"].getName())
+            return render_template("index.html", user=playerpool["player1"])
         elif "player2" not in playerpool:
             session["username"] = "player2"
             playerpool["player2"] = player("player2")
             print("born player2")
-            return render_template("index.html",user=playerpool["player2"])
+            return render_template("index.html", user=playerpool["player2"])
         elif "player3" not in playerpool:
             session["username"] = "player3"
             playerpool["player3"] = player("player3")
             print("born player3")
-            return render_template("index.html",user=playerpool["player3"])
+            return render_template("index.html", user=playerpool["player3"])
     else:
         username = session["username"]
-        print("use",username)
+        print("use", username)
         print(playerpool[username].getName())
-        return render_template("index.html",user=playerpool[username])
+        return render_template("index.html", user=playerpool[username])
     
 
-@app.route('/',methods = ["POST"])
+@app.route('/', methods = ["POST"])
 def formHandle():
     username = session["username"]
     name = request.values.get('direction')
     print("Action" + name)
     return render_template("index.html",user=playerpool[username])
+
 
 if __name__ == "__main__":
     app.debug = True
