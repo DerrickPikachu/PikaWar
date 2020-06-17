953ef31e478eb1131dcc195805d92b410d52e3fd
oscar1234456 <chen.oscar@hotmail.com>
2020-06-17 17:11:25 +0800

    [modify]items function after using,  items will remove from user items

--- Game/item.py
+++ Game/item.py
@@ -2,7 +2,8 @@ from Game.player import player
 items = ["狙擊槍", "醫療箱", "步槍", "防彈衣"]
 
 
-def sniperRifle(user1:player, user2:player, user3:player, location:list):
+def sniperRifle(user1:player, user2:player, user3:player, userIndex:int,location:list):
+    users = [user1, user2, user3]
     print("sniperRifle Active!")
     if user1.getPosition() == location:
         print("sniperRifle user1 is attacked!")
@@ -10,9 +11,12 @@ def sniperRifle(user1:player, user2:player, user3:player, location:list):
     elif user2.getPosition() == location:
         print("sniperRifle user2 is attacked!")
         user2.setBlood(user2.getBlood()//2)
-    else:
+    elif user3.getPosition() == location:
         print("sniperRifle user3 is attacked!")
         user3.setBlood(user3.getBlood()//2)
+    else:
+        print("nobody will be hurt!")
+    users[userIndex].items.remove(0)
 
 
 def medical(user:player): 
@@ -21,6 +25,7 @@ def medical(user:player):
         user.setBlood(10)
     else:
         user.setBlood(totalBlood)
+    user.items.remove(1)
 
 
 def rifle(user:player):
@@ -29,6 +34,7 @@ def rifle(user:player):
         user.setPower(10)
     else:
         user.setPower(totalPower)
+    
 
 
 def armor(user:player):
@@ -37,4 +43,5 @@ def armor(user:player):
         user.setDefense(7)
     else:
         user.setDefense(totalDefense)
+    
     
