1152451db36e9ccf9083fee237704edafdc5a1fd
oscar1234456 <chen.oscar@hotmail.com>
2020-06-17 12:11:53 +0800

    [modify]moveOn delete the items on

--- Game/gameMap.py
+++ Game/gameMap.py
@@ -18,7 +18,7 @@ class GameMap:
         if self.blocks[position[0]][position[1]] != "":
             itemsMap = {"狙擊槍": 0, "醫療箱":1, "步槍":2, "防彈衣":3}
             user.items.add(itemsMap[self.blocks[position[0]][position[1]]])
-
+            self.blocks[position[0]][position[1]] = ""
 
 if __name__ == '__main__':
     gameMap = GameMap()
